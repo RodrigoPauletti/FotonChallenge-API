085307037eb10fb202e03a3a7629b02493a24997
Rodrigo Pauletti <digopauletti@yahoo.com.br>
2020-01-06 18:51:28 -0300

    Removing unused headers

--- server.js
+++ server.js
@@ -9,16 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-});
-
 dotenv.config();
 
 // Starting DB
